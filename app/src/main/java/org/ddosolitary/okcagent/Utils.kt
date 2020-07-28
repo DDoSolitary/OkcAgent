ffe7ecd7647ba83e0ea239c0d64d3f9bf124562b
DDoSolitary <DDoSolitary@gmail.com>
2020-07-28 16:45:40 +0800

    Show error as notification when in background.

--- app/src/main/java/org/ddosolitary/okcagent/Utils.kt
+++ app/src/main/java/org/ddosolitary/okcagent/Utils.kt
@@ -1,14 +1,39 @@
 package org.ddosolitary.okcagent
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import java.io.OutputStream
 
 fun showError(context: Context, msg: String) {
-	context.startActivity(Intent(context, ErrorDialogActivity::class.java).apply {
-		flags = Intent.FLAG_ACTIVITY_NEW_TASK
-		putExtra(EXTRA_ERROR_MESSAGE, msg)
-	})
+	if (context is Service) {
+		val mgr = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			mgr.createNotificationChannel(
+				NotificationChannel(
+					context.getString(R.string.channel_id_error),
+					context.getString(R.string.channel_error),
+					NotificationManager.IMPORTANCE_HIGH
+				)
+			)
+		}
+		val notification = NotificationCompat.Builder(context, context.getString(R.string.channel_id_error))
+			.setPriority(NotificationCompat.PRIORITY_HIGH)
+			.setSmallIcon(R.drawable.ic_error)
+			.setContentTitle(context.getString(R.string.text_error))
+			.setContentText(msg)
+			.build()
+		mgr.notify(context.resources.getInteger(R.integer.notification_id_error), notification)
+	} else {
+		context.startActivity(Intent(context, ErrorDialogActivity::class.java).apply {
+			flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			putExtra(EXTRA_ERROR_MESSAGE, msg)
+		})
+	}
 }
 
 fun showError(context: Context, resId: Int) = showError(context, context.getString(resId))
