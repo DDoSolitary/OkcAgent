a70f735bc5fb73a921d840245d8ada2f68b98c90
DDoSolitary <DDoSolitary@gmail.com>
2020-08-18 19:51:10 +0800

    Update dependencies.

--- app/src/main/java/org/ddosolitary/okcagent/ssh/SshKeyInfo.kt
+++ app/src/main/java/org/ddosolitary/okcagent/ssh/SshKeyInfo.kt
@@ -2,9 +2,8 @@ package org.ddosolitary.okcagent.ssh
 
 import android.content.Context
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.list
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 import org.ddosolitary.okcagent.R
 
 @Serializable
@@ -13,12 +12,12 @@ data class SshKeyInfo(val id: String, val description: String) {
 		fun load(context: Context): List<SshKeyInfo> {
 			val pref = context.getSharedPreferences(context.getString(R.string.pref_main), Context.MODE_PRIVATE)
 			val json = pref.getString(context.getString(R.string.key_ssh_keys), null) ?: "[]"
-			return Json(JsonConfiguration.Stable).parse(serializer().list, json)
+			return Json.decodeFromString(ListSerializer(serializer()), json)
 		}
 
 		fun save(list: List<SshKeyInfo>, context: Context) {
 			val pref = context.getSharedPreferences(context.getString(R.string.pref_main), Context.MODE_PRIVATE)
-			val json = Json(JsonConfiguration.Stable).stringify(serializer().list, list)
+			val json = Json.encodeToString(ListSerializer(serializer()), list)
 			pref.edit().apply {
 				putString(context.getString(R.string.key_ssh_keys), json)
 				apply()
